2ad7b6a605106b6a998d43f35749ddb9f3af65c9
taeheongo <taeheon9966@icloud.com>
2020-11-20 17:36:23 +0900

    test code for api callback

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const path = require("path");
+const axios = require("axios");
 
 const app = express();
 
 const PORT = 4001;
 
 app.use(express.static(path.join(__dirname, "public/build")));
+
+app.get("/auth/oauth", (req, res, next) => {
+  // This route is callback for making app test
+  const { code } = req.query;
+
+  console.log("code:", code);
+
+  res.end();
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/build/index.html"));
